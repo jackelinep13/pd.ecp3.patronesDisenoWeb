aa6ebbd8c41ca70ce2e95c6859899d3b3059d0b5
jackeline <jackeline@JACKELINEP>
2014-10-24 01:29:51 +0200

    Creacion del paquete MemDAO, fichero MemDAOFactory

---
+++ src/main/java/models/DAO/mem/MemDAOFactory.java
@@ -0,0 +1,21 @@
+package models.DAO.mem;
+
+import models.DAO.DAOFactory;
+import models.DAO.RoundDAO;
+import models.DAO.UserDAO;
+
+public class MemDAOFactory extends DAOFactory{
+
+	@Override
+	public UserDAO getUserDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RoundDAO getRoundDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
